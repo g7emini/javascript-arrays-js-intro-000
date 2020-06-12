5c1a78bc74c21cc14735b6d01bdc8a9bfc869a25
groana <contact@groanamelendez.com>
2020-06-12 19:39:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,8 @@ function accessElementInArray(arr, n) {
 
 
 function destructivelyRemoveElementFromBeginningOfArray(arr){
-  return arr.shift();
+  arr.shift();
+  return arr;
 }
 
 function removeElementFromBeginningOfArray(arr){
