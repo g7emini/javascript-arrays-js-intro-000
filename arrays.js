1cfec117d3da174f0d7b6fddde8744827d9c2f71
groana <contact@groanamelendez.com>
2020-06-12 18:58:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,19 +5,24 @@ var chocolateBars = [
   "skittles"
   ];
   
+  
 function addElementToBeginningOfArray(arr, el) {
   return [el, ...arr]; 
 }
+
 
 function destructivelyAddElementToBeginningOfArray(arr, el){
   arr.unshift(el);
   return arr;
 }
 
+
 function addElementToEndOfArray(arr, el){
   return [...arr, el];
 }
 
+
 function destructivelyAddElementToEndOfArray(arr, el){
   arr.push(el);
 }
+
